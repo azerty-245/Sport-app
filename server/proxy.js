5bb4e21b4ff2334a52afd1e6f7bafb51c26ffdd1
azerty-245 <akakpoebenezer03@gmail.com>
2026-02-17 21:56:11 +0100

    fix: optimize buffer & recovery

--- server/proxy.js
+++ server/proxy.js
@@ -107,8 +107,8 @@ class Broadcaster {
         this.ffmpeg = spawn('ffmpeg', [
             '-reconnect', '1', '-reconnect_at_eof', '1', '-reconnect_streamed', '1',
             '-reconnect_on_network_error', '1', '-reconnect_on_http_error', '4xx,5xx',
-            '-reconnect_delay_max', '5',
-            '-rw_timeout', '10000000',
+            '-reconnect_delay_max', '2',
+            '-rw_timeout', '5000000',
             '-multiple_requests', '1',
             '-fflags', '+genpts+igndts+discardcorrupt',
             '-err_detect', 'ignore_err',
