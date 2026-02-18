2cf9f3edd4104a17e33cf28f8ae60d7de52bb6f3
azerty-245 <akakpoebenezer03@gmail.com>
2026-02-18 10:40:23 +0100

    add new monetag

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,6 @@
 self.options = {
     "domain": "5gvci.com",
-    "zoneId": 10613584
+    "zoneId": 10624116
 }
 self.lary = ""
 importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
